880003d2ae940632fa32ca7976e786262a476f4b
vbazhutin <vladimir.bazhutin@gmail.com>
2022-07-26 17:25:31 -0500

    hardhat and helper configs

---
+++ helper-hardhat-config.js
@@ -0,0 +1,19 @@
+const MIN_DELAY = 3600
+const VOTING_PERIOD = 5
+const VOTING_DELAY = 1
+const QUORUM_PERCENTAGE = 4
+const FUNCTION = "store"
+const NEW_STORE_VALUE = 55
+const PROPOSAL_DESCRIPTION = "Proposal #1: Store 55 in the Box contract"
+const proposalsFile = "proposals.json"
+
+module.exports = {
+    MIN_DELAY,
+    VOTING_PERIOD,
+    VOTING_DELAY,
+    QUORUM_PERCENTAGE,
+    FUNCTION,
+    NEW_STORE_VALUE,
+    PROPOSAL_DESCRIPTION,
+    proposalsFile,
+}
